cbdca7ccb3413f2c2fcee49fa6a6a517bc5e57c3
FCem <fcemcihan@gmail.com>
2016-04-27 16:40:57 +0300

    delete_sınıfı _eklendi
    delete sınıfı ekleyıp commıt ediyorum

---
+++ src/com/fcem/cihan/crod/islemleri/Delete.java
@@ -0,0 +1,13 @@
+package com.fcem.cihan.crod.islemleri;
+
+public class Delete {
+	public static void main(String[] args) {
+		System.out.println("sdaf scd");
+		System.out.println("sdaf scd");
+		System.out.println("sdaf scd");
+		System.out.println("sdaf scd");
+		System.out.println("sdaf scd");
+		//sdfkhjnhljh
+	}
+
+}
